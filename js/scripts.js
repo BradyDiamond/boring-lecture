1fa0da71a7d5754e99abcd061170f5d5317f08a7
Brady Diamond <brdydmnd@gmail.com>
2022-03-22 14:31:07 -0700

    WIP light/dark highlighting
    Co-authored-by: jarretjeter <jarretjeter@yahoo.com

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,5 @@ $(document).ready(function() {
     $("p").removeClass();
     $("p").addClass("dark");
   });
-
-  $("p.light").click(function() {
-    $("p").removeClass();
-    $("p").addClass("light");
-  });
-
-  $("p.dark").click(function() {
-    $("p").removeClass();
-    $("p").addClass("dark");
-  });
+ 
 });  
